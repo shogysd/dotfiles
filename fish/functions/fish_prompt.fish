687a69273488e41e195e37a40c03f4a8c0679b4e
sh-o <shog.ysd@gmail.com>
2020-06-23 01:46:54 +0900

    Create fish_prompt.fish

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,57 @@
+function fish_prompt --description 'Write out the prompt'
+    set -l last_pipestatus $pipestatus
+    set -l normal (set_color normal)
+
+    # Color the prompt differently when we're root
+    set -l color_cwd $fish_color_cwd
+    set -l prefix
+    set -l suffix '$'
+
+    if contains -- $USER root toor
+        if set -q fish_color_cwd_root
+            set color_cwd $fish_color_cwd_root
+        end
+        set suffix '#'
+    end
+
+    # If we're running via SSH, change the host color.
+    set -l color_host $fish_color_host
+    if set -q SSH_TTY
+        set color_host $fish_color_host_remote
+    end
+
+    # Write pipestatus
+    set -l prompt_status (__fish_print_pipestatus "[" "] " "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+
+    echo ""
+    echo -s "$USER"  @ (prompt_hostname) ' ( ' (_prompt_pwd) ' )' $normal
+    echo -s $prompt_status $suffix " "
+end
+
+
+# Defined in /usr/local/share/fish/functions/prompt_pwd.fish @ line 1
+function _prompt_pwd --description 'Print the current working directory, shortened to fit the prompt'
+    set -l options 'h/help'
+    argparse -n prompt_pwd --max-args=0 $options -- $argv
+    or return
+
+    if set -q _flag_help
+        __fish_print_help prompt_pwd
+        return 0
+    end
+
+    # This allows overriding fish_prompt_pwd_dir_length from the outside (global or universal) without leaking it
+    set -q fish_prompt_pwd_dir_length
+    or set -l fish_prompt_pwd_dir_length 3
+
+    # Replace $HOME with "~"
+    set realhome ~
+    set -l tmp (string replace -r '^'"$realhome"'($|/)' '~$1' $PWD)
+
+    if [ $fish_prompt_pwd_dir_length -gt (echo $tmp | wc -c) ]
+        echo $tmp
+    else
+        # Shorten to at most $fish_prompt_pwd_dir_length characters per directory
+        string replace -ar '(\.?[^/]{'"$fish_prompt_pwd_dir_length"'})[^/]*/' '$1_/' $tmp
+    end
+end
