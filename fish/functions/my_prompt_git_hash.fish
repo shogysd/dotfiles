6166df8a9096b8fb0520b447b86353368f7c3655
sh-o <shog.ysd@gmail.com>
2020-06-27 04:28:12 +0900

    Create my_prompt_git_hash.fish

---
+++ fish/functions/my_prompt_git_hash.fish
@@ -0,0 +1,8 @@
+function my_prompt_git_hash
+    set -l git_hash (git rev-parse --short HEAD 2> /dev/null)
+    if [ $status -eq 0 ]
+        echo -n $git_hash
+    else
+        echo -n "Not committed"
+    end
+end
