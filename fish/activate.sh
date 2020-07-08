cc8ac7675e630d38d1bdc56689a938c8f04a4d2e
Shogo Yasuda <shog.ysd@gmail.com>
2020-07-09 01:55:32 +0900

    up

---
+++ fish/activate.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+echo "update fish configs symbolic links"
+echo    "    fish/functions"   ; rm -rf ~/.config/fish/functions   ; ln -s ~/config_files/fish/functions       ~/.config/fish/functions
+echo    "    fish/imports"     ; rm -rf ~/.config/fish/imports     ; ln -s ~/config_files/fish/imports         ~/.config/fish/imports
+echo    "    fish/config.fish" ; rm -rf ~/.config/fish/config.fish ; ln -s ~/config_files/fish/config.fish     ~/.config/fish/config.fish
