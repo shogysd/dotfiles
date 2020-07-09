7a0d56470b26c3d1a84463509f8460b92146dcac
sh-o <shog.ysd@gmail.com>
2020-07-10 01:34:37 +0900

    Update activate.sh

--- gnome_terminal/activate.sh
+++ gnome_terminal/activate.sh
@@ -1,3 +1,3 @@
-#! /bin/sh
+#! env bash
 echo "update gnome-terminal configs"
 dconf reset -f /org/gnome/terminal/legacy/profiles:/ ; dconf load /org/gnome/terminal/legacy/profiles:/ < ~/config_files/gnome_terminal/gnome-terminal.profile
