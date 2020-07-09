2e95979f2ef3cdb2c2591087e0eb1d67129d3248
sh-o <shog.ysd@gmail.com>
2020-07-10 02:17:42 +0900

    Update activate.sh

--- gnome_terminal/activate.sh
+++ gnome_terminal/activate.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
+
+if [ `uname 2>&1` = "Darwin" ]; then
+    # macOS
+    exit
+fi
+
 echo "update gnome-terminal configs"
 dconf reset -f /org/gnome/terminal/legacy/profiles:/ ; dconf load /org/gnome/terminal/legacy/profiles:/ < ~/config_files/gnome_terminal/gnome-terminal.profile
