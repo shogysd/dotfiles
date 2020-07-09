5dc90585a2523bd9e1453c8f83ea6f3df99381d4
Shogo Yasuda <shog.ysd@gmail.com>
2020-07-10 00:33:37 +0900

    up

--- bash/activate.sh
+++ bash/activate.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 echo "update bash configs symbolic links"
-echo "    bash_profile" ; rm -f ~/.bash_profile ; ln -s ~/config_files/bash_conf/src/bash_profile ~/.bash_profile
-echo "    bashrc"       ; rm -f ~/.bashrc       ; ln -s ~/config_files/bash_conf/src/bashrc       ~/.bashrc
+echo "    bash_profile" ; rm -f ~/.bash_profile ; ln -s ~/config_files/bash/src/bash_profile ~/.bash_profile
+echo "    bashrc"       ; rm -f ~/.bashrc       ; ln -s ~/config_files/bash/src/bashrc       ~/.bashrc
