1b620f7b81ef1986ead7d4d8b1b958b34e6b63ca
sh-o <shog.ysd@gmail.com>
2020-07-10 01:32:33 +0900

    Update activate.sh

--- bash/activate.sh
+++ bash/activate.sh
@@ -1,4 +1,4 @@
-#! /bin/sh
+#! env bash
 echo "update bash configs symbolic links"
 echo "    bash_profile" ; rm -f ~/.bash_profile ; ln -s ~/config_files/bash/bash_profile ~/.bash_profile
 echo "    bashrc"       ; rm -f ~/.bashrc       ; ln -s ~/config_files/bash/bashrc       ~/.bashrc
