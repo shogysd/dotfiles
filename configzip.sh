08f400b97c6b3f8261c0c808c2ced7a87542f111
Shogo Yasuda <shog.ysd@gmail.com>
2020-07-12 01:32:30 +0900

    up

--- configzip.sh
+++ configzip.sh
@@ -3,12 +3,14 @@
 if [ ! -d ~/config_files ]; then
     mkdir ~/config_files
 fi
-pushd ~/config_files
-rm -rf zips; mkdir zips
+pushd ~/config_files > /dev/null
+zips_dirname=zips-$(date +"%Y%m%d_%H%M%S")
+rm -rf zips*; mkdir ${zips_dirname}
 
 for i in $(ls -d ~/config_files/*/ | grep -v 'zips' | xargs basename)
 do
-    zip -r ~/config_files/zips/${i}.zip ${i}
+    zip -rq ${zips_dirname}/${i}.zip ${i}
 done
 
-popd
+unset zips_dirnam
+popd > /dev/null
