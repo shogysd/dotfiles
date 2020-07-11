69f5a7004e328a78dbedf761d9b33167a1582213
Shogo Yasuda <shog.ysd@gmail.com>
2020-07-12 00:55:52 +0900

    up

--- git/activate.sh
+++ git/activate.sh
@@ -37,8 +37,9 @@ git   config --global user.name  "${git_user_name}"
 git   config --global user.email "${git_user_email}"
 mv    ~/.gitconfig ~/.gitconfig_userinfo
 unset git_user_name git_user_email git_user_name_read git_user_email_read update reset_flag
-echo    ""
-echo    "update symbolic link"
-echo    "    gitconfig_envdep_ln" ; rm -f ~/config_files/git/gitconfig_envdep_ln ; ln -s ~/config_files/git/$(echo $(uname 2>&1) | tr [A-Z] [a-z])/gitconfig_envdep ~/config_files/git/gitconfig_envdep_ln
-echo    "    gitconfig"           ; rm -f ~/.gitconfig                           ; ln -s ~/config_files/git/gitconfig        ~/.gitconfig
-echo    "    gitignore_global"    ; rm -f ~/.gitignore_global                    ; ln -s ~/config_files/git/gitignore_global ~/.gitignore_global
+echo ""
+echo "update symbolic link"
+echo "    gitconfig_envdep_ln" ; rm -f ~/config_files/git/gitconfig_envdep_ln ; ln -s ~/config_files/git/$(echo $(uname 2>&1) | tr [A-Z] [a-z])/gitconfig_envdep ~/config_files/git/gitconfig_envdep_ln
+echo "    gitconfig"           ; rm -f ~/.gitconfig                           ; ln -s ~/config_files/git/gitconfig        ~/.gitconfig
+echo "    gitignore_global"    ; rm -f ~/.gitignore_global                    ; ln -s ~/config_files/git/gitignore_global ~/.gitignore_global
+echo "download git completion" ; rm -f ~/config_files/git/git-completion.bash ; ${MY_DOWNLOAD_COMMAND} ~/config_files/git/git-completion.bash https://raw.githubusercontent.com/git/git/d9f6f3b6195a0ca35642561e530798ad1469bd41/contrib/completion/git-completion.bash
