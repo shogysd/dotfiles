26a68b1f3e8c5a344233150a639a460a630b08b6
sh-o <shog.ysd@gmail.com>
2020-07-28 01:43:32 +0900

    Update activate.sh

--- fish/activate.sh
+++ fish/activate.sh
@@ -3,3 +3,7 @@ echo "update fish configs symbolic links"
 echo    "    fish/functions"   ; rm -rf ~/.config/fish/functions   ; ln -s ~/dotfiles/fish/functions       ~/.config/fish/functions
 echo    "    fish/imports"     ; rm -rf ~/.config/fish/imports     ; ln -s ~/dotfiles/fish/imports         ~/.config/fish/imports
 echo    "    fish/config.fish" ; rm -rf ~/.config/fish/config.fish ; ln -s ~/dotfiles/fish/config.fish     ~/.config/fish/config.fish
+
+# default shell setup
+# check: /etc/shells
+# $ chsh -s [fish PATH]
