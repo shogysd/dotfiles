13ce915f97585d673cefa1482319851e41ee697e
Shogo Yasuda <shog.ysd@gmail.com>
2020-08-30 00:59:30 +0900

    up

--- fish/activate.sh
+++ fish/activate.sh
@@ -6,10 +6,4 @@ if [ ! -d ~/.config ]; then
 fi
 
 echo "update fish configs symbolic links"
-echo    "    fish/functions"   ; rm -rf ~/.config/fish/functions   ; ln -s ~/dotfiles/fish/functions       ~/.config/fish/functions
-echo    "    fish/imports"     ; rm -rf ~/.config/fish/imports     ; ln -s ~/dotfiles/fish/imports         ~/.config/fish/imports
-echo    "    fish/config.fish" ; rm -rf ~/.config/fish/config.fish ; ln -s ~/dotfiles/fish/config.fish     ~/.config/fish/config.fish
-
-# default shell setup
-# check: /etc/shells
-# $ chsh -s [fish PATH]
+echo "    fish/"; rm -rf ~/.config/fish; ln -s ~/dotfiles/fish/fish ~/.config/fish
