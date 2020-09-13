a9ea9cbf09d8c150f42b5175921de6f4de458340
Shogo Yasuda <shog.ysd@gmail.com>
2020-09-14 00:41:03 +0900

    up

--- fish/fish/functions/python_latest_stable_binpath.fish
+++
@@ -1,9 +0,0 @@
-function python_latest_stable_binpath
-    for path in (find /Library/Frameworks/Python.framework/Versions/* -maxdepth 1 -name bin | sort -r)
-        $path/python3 --version | awk -F ' ' '{print $NF}' | grep -e a -e b > /dev/null
-        if [ ! $status -eq 0 ]
-            echo $path
-            break
-        end
-    end
-end
