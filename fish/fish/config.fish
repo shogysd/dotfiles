6ad89d4309bcb93e5a332d574f02d5325d7cd740
Shogo Yasuda <shog.ysd@gmail.com>
2020-09-23 01:51:27 +0900

    up

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -11,27 +11,27 @@ else
 end
 
 
-if [ -f '~/.config/fish/imports/environments.fish' ];
-    . '~/.config/fish/imports/environments.fish'
+if [ -f ~/.config/fish/imports/environments.fish ];
+    . ~/.config/fish/imports/environments.fish
 else
     echo "~/.config/fish/imports/environments.fish : No such file"
 end
 
-if [ -f '~/.config/fish/imports/aliases.fish' ];
-    . '~/.config/fish/imports/aliases.fish'
+if [ -f ~/.config/fish/imports/aliases.fish ];
+    . ~/.config/fish/imports/aliases.fish
 else
     echo "~/.config/fish/imports/aliases.fish : No such file"
 end
 
-if [ -f '~/dotfiles/docker/docker.fish' ];
-    . '~/dotfiles/docker/docker.fish'
+if [ -f ~/dotfiles/docker/docker.fish ];
+    . ~/dotfiles/docker/docker.fish
 else
     echo "~/dotfiles/docker/docker.fish : No such file"
 end
 
 # gcloud
-if [ -f '~/google-cloud-sdk/path.fish.inc' ];
-    . '~/google-cloud-sdk/path.fish.inc'
+if [ -f ~/google-cloud-sdk/path.fish.inc ];
+    . ~/google-cloud-sdk/path.fish.inc
 else
     echo "~/google-cloud-sdk/path.fish.inc : No such file"
 end
