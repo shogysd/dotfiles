28667b0eba83d267dcc097858e180dc78d84a2d7
Shogo Yasuda <shog.ysd@gmail.com>
2020-11-02 17:13:30 +0900

    up

--- posix_shell/common/cmd_wrapper.sh
+++ posix_shell/common/cmd_wrapper.sh
@@ -5,20 +5,3 @@ else
     # Linux
     :
 fi
-
-function pip(){
-    if [ -z ${VIRTUAL_ENV} ]; then
-        echo -ne "${OSDEP_ESC_CODE}[1;33mWARNING: ${OSDEP_ESC_CODE}[0;39m"
-        echo "virtualenv is not activated!!"
-        echo -ne "${OSDEP_ESC_CODE}[1;39mcontinue ?: ${OSDEP_ESC_CODE}[0;39m"
-        echo -n "y-or-other: "
-        read start
-        if [ -z ${start} ] || [ "${start}" != "y" ]; then
-            unset start
-            return 1
-        fi
-        unset start
-        $(which python3) -m pip ${@}
-        return $?
-    fi
-}
--- posix_shell/zsh/activate.sh
+++ posix_shell/zsh/activate.sh
@@ -11,3 +11,7 @@ fi
 echo "update zsh configs symbolic links"
 echo "    zprofile" ; rm -f ~/.zprofile ; ln -s ~/dotfiles/posix_shell/zsh/zprofile ~/.zprofile
 echo "    zshrc"    ; rm -f ~/.zshrc    ; ln -s ~/dotfiles/posix_shell/zsh/zshrc    ~/.zshrc
+
+echo "plugin repository clone"
+echo "    zsh-autosuggestions"     ; rm -rf ~/.zsh/zsh-autosuggestions     ; git clone https://github.com/zsh-users/zsh-autosuggestions     ~/.zsh/zsh-autosuggestions
+echo "    zsh-syntax-highlighting" ; rm -rf ~/.zsh/zsh-syntax-highlighting ; git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
