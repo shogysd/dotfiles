6f7858cf7400eb5f7d163a2cfcda70dfbba8c0a9
Shogo Yasuda <shog.ysd@gmail.com>
2020-11-04 01:44:47 +0900

    up

--- posix_shell/common/activate.sh
+++ posix_shell/common/activate.sh
@@ -7,7 +7,3 @@ else
     # Linux
     export OSDEP_ESC_CODE='\e'
 fi
-
-echo "update bash configs symbolic links"
-echo "    bash_profile" ; rm -f ~/.bash_profile ; ln -s ~/dotfiles/bash/bash_profile ~/.bash_profile
-echo "    bashrc"       ; rm -f ~/.bashrc       ; ln -s ~/dotfiles/bash/bashrc       ~/.bashrc
