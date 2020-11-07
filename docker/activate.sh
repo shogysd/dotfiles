05e721673ff081b11daeb61cbfe51889e4ad4aae
Shogo Yasuda <shog.ysd@gmail.com>
2020-11-07 23:37:05 +0900

    up

--- docker/activate.sh
+++ docker/activate.sh
@@ -25,6 +25,6 @@ mkdir -p ~/dotfiles/zsh/completion
 #     mkdir ${HOME}/dotfiles/zsh/completion && \
 #     echo "$HOME/dotfiles/zsh/completion was created."
 # fi
-rm -rf ~/dotfiles/zsh/completion/_docker ; ln -s ~/dotfiles/docker/docker.zsh ~/dotfiles/zsh/completion/_docker
+rm -rf ~/dotfiles/zsh/completion/_docker ; ln -s ~/dotfiles/docker/docker.zsh ~/dotfiles/posix_shell/zsh/completion/_docker
 
 unset bash_url fish_url
