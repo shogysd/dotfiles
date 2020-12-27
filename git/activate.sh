9e4ef64f9fa3fc6edb1a29f4982782b6fb1f985e
Shogo Yasuda <shog.ysd@gmail.com>
2020-12-28 02:45:56 +0900

    up

--- git/activate.sh
+++ git/activate.sh
@@ -48,7 +48,6 @@ mv    ~/.gitconfig ~/.gitconfig_userinfo
 unset git_user_name git_user_email git_user_name_read git_user_email_read update reset_flag
 echo ""
 echo "update symbolic link"
-echo "    gitconfig_envdep_ln"        ; rm -f ~/dotfiles/git/gitconfig_envdep_ln ; ln -s ~/dotfiles/git/$(echo "$(uname 2>&1)" | tr [A-Z] [a-z])/gitconfig_envdep ~/dotfiles/git/gitconfig_envdep_ln
 echo "    gitconfig"                  ; rm -f ~/.gitconfig                           ; ln -s ~/dotfiles/git/gitconfig        ~/.gitconfig
 echo "    gitignore_global"           ; rm -f ~/.gitignore_global                    ; ln -s ~/dotfiles/git/gitignore_global ~/.gitignore_global
 
