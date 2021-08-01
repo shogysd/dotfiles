f5f30454efefb11aea08969f21b122e12fb17502
Shogo Yasuda <shog.ysd@gmail.com>
2021-08-02 01:39:20 +0900

    up

--- posix_shell/common/activate.sh
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env bash
-
-if [ "$(uname 2>&1)" = "Darwin" ]; then
-    # macOS
-    export OSDEP_ESC_CODE='\033'
-else
-    # Linux
-    export OSDEP_ESC_CODE='\e'
-fi
--- posix_shell/common/cmd_wrapper.sh
+++
@@ -1,7 +0,0 @@
-if [ "$(uname 2>&1)" = "Darwin" ]; then
-    # macOS
-    :
-else
-    # Linux
-    :
-fi
